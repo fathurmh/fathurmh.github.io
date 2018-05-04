3f6a835fd23701ab42ab845a01e182b29da181cb
fathurmh <fathur.mh@gmail.com>
2018-05-04 22:07:10 +0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# fathurmh.github.io
---
+++ js/init.js
@@ -0,0 +1,111 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Init JS
+/*
+-----------------------------------------------------------------------------------*/
+
+ jQuery(document).ready(function($) {
+
+/*----------------------------------------------------*/
+/* FitText Settings
+------------------------------------------------------ */
+
+    setTimeout(function() {
+	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '60px' });
+	 }, 100);
+
+
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+
+/*----------------------------------------------------*/
+/* Highlight the current section in the navigation bar
+------------------------------------------------------*/
+
+	var sections = $("section");
+	var navigation_links = $("#nav-wrap a");
+  var tes = $('ul#nav li a');
+  var nav = $('#nav-wrap');
+
+	sections.waypoint({
+    handler: function(event, direction) {
+      var active_section;
+			active_section = $(this);
+			if (direction === "up") active_section = active_section.prev();
+      if (active_section.attr("id") === "home") {
+        // tes.removeClass('curr').fadeIn('fast');
+        nav.removeClass('opaque').fadeIn('slow');
+      } else {
+        // tes.addClass('curr').fadeIn('fast');
+        nav.addClass('opaque').fadeIn('slow');
+      };
+			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+      navigation_links.parent().removeClass("current");
+			active_link.parent().addClass("current");
+		},
+		offset: '35%'
+	});
+
+
+/*----------------------------------------------------*/
+/*	Make sure that #header-background-image height is
+/* equal to the browser height.
+------------------------------------------------------ */
+
+   $('header').css({ 'height': $(window).height() });
+   $(window).on('resize', function() {
+
+        $('header').css({ 'height': $(window).height() });
+        $('body').css({ 'width': $(window).width() })
+   });
+
+/*----------------------------------------------------*/
+/*	Modal Popup
+------------------------------------------------------*/
+
+    $('.item-wrap a').magnificPopup({
+
+       type:'inline',
+       fixedContentPos: false,
+       removalDelay: 200,
+       showCloseBtn: false,
+       mainClass: 'mfp-fade'
+
+    });
+
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+    		e.preventDefault();
+    		$.magnificPopup.close();
+    });
+
+
+/*----------------------------------------------------*/
+/*	Flexslider
+/*----------------------------------------------------*/
+   $('.flexslider').flexslider({
+      namespace: "flex-",
+      controlsContainer: ".flex-container",
+      animation: 'slide',
+      controlNav: true,
+      directionNav: false,
+      smoothHeight: true,
+      slideshowSpeed: 7000,
+      animationSpeed: 600,
+      randomize: false,
+   });
+});
